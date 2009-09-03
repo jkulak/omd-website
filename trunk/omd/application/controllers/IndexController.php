9e9df536b329ad66923a7fdb03ce576fd08f3054
Jakub.Kulak <Jakub.Kulak@eabb647c-98d7-11de-9e98-d1f6c90d23ca>
2009-09-03 22:25:41 +0000

    + pierwsze wgranie plikow

---
+++ trunk/omd/application/controllers/IndexController.php
@@ -0,0 +1,6 @@
+<?php
+class IndexController extends Zend_Controller_Action {
+   function indexAction() {
+      $this->view->title = "OMD"; 
+   }
+}
---
+++ trunk/omdform/application/controllers/IndexController.php
@@ -0,0 +1,6 @@
+<?php
+class IndexController extends Zend_Controller_Action {
+   function indexAction() {
+      $this->view->title = "Panel administracyjny OMD, czyli super formatka ExtJS od Stanioka! Yeah."; 
+   }
+}
