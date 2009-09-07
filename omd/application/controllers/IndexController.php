50fb112c93563fd4dce601cfb25b6db3cd460b8b
Jakub.Kulak <Jakub.Kulak@eabb647c-98d7-11de-9e98-d1f6c90d23ca>
2009-09-07 23:27:31 +0000

    + dodalem puste podstrony

--- omd/application/controllers/IndexController.php
+++ omd/application/controllers/IndexController.php
@@ -1,6 +1,44 @@
 <?php
 class IndexController extends Zend_Controller_Action {
-   function indexAction() {
-      $this->view->title = "OMD"; 
+	function init() {
+		$this->view->baseUrl = $this->_request->getBaseUrl();
+	} 
+   
+	function indexAction() {
+	
+	}
+
+   function kontaktAction() {
    }
+
+   function onasAction() {
+   }
+
+	function aktualnosciAction() {
+		
+	}
+	
+	function zespolAction() {
+		
+	}
+	
+	function naszepraceAction() {
+		
+	}
+	
+	function narzedziaAction() {
+		
+	}
+	
+	function analizyipublikacjeAction() {
+		
+	}
+	
+	function omdschoolAction() {
+		
+	}
+	
+	function siecomdAction() {
+		
+	}
 }
