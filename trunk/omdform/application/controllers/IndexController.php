062a39f5bb293436447d456b1ad24b0ef4b1ac46
a3J6eXM <a3J6eXM@eabb647c-98d7-11de-9e98-d1f6c90d23ca>
2009-09-08 21:21:14 +0000

    frame ext-a

--- trunk/omdform/application/controllers/IndexController.php
+++ trunk/omdform/application/controllers/IndexController.php
@@ -3,7 +3,12 @@ class IndexController extends Zend_Controller_Action
 {
 
 	function indexAction() {
-   		$article = new Dupa_Article();
+   		//$article = new Dupa_Article();
+   		/**
+   		 * #TODO gdzie jest config?
+   		 */
+		$this->view->publicUrl = 'http://localhost/omdweb/omdweb/omdform/public/';
+		
    			   	
 		$this->view->title = "Panel administracyjny OMD, czyli super formatka ExtJS od Stanioka! Yeah."; 
 	}
---
+++ trunk/omdform/public/scripts/omd/Omd.Langpl.js
@@ -0,0 +1,13 @@
+(function(){
+	
+	Ext.namespace( 'Omd.Langpl' );
+	
+	var _lang = Omd.Langpl;
+	
+	//tytuł zakładki artykuły
+	_lang.TabTitleArticle = 'Artykuły';
+
+	//tytuł zakładki kategorie
+	_lang.TabTitleCategories = 'Kategorie';
+
+})();
