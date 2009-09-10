4650b1187ae2af14f208ad34b3824ff5c5cb10d6
a3J6eXM <a3J6eXM@eabb647c-98d7-11de-9e98-d1f6c90d23ca>
2009-09-10 18:40:38 +0000

    front formatki gotowy

--- omdform/application/controllers/IndexController.php
+++ omdform/application/controllers/IndexController.php
@@ -1,15 +1,63 @@
 <?php
 class IndexController extends Zend_Controller_Action 
 {
+	public function init() {
+   		/**
+   		 * #TODO gdzie jest config?
+   		 */
+		//$this->view->publicUrl = 'http://webascrazy.net/omdform/public/';
+		$this->view->publicUrl = 'http://localhost/omdweb/omdweb/omdform/public/';
+		
+		//$this->view->apiUrl = 'http://webascrazy.net/omdform/';
+		$this->view->apiUrl = 'http://localhost/omdweb/omdweb/omdform/';
+   			   	
+		$this->view->title = "Panel administracyjny OMD, czyli super formatka ExtJS od Stanioka! Yeah."; 
+	}
 
-	function indexAction() {
+	public function indexAction() {
    		//$article = new Dupa_Article();
    		/**
    		 * #TODO gdzie jest config?
    		 */
-		$this->view->publicUrl = 'http://localhost/omdweb/omdweb/omdform/public/';
+	}
+	
+	public function getarticlelistAction() {
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender();
 		
-   			   	
-		$this->view->title = "Panel administracyjny OMD, czyli super formatka ExtJS od Stanioka! Yeah."; 
+		$articleList = '{'.
+			'result : {'.
+				'items : ['.
+					'{ id : 1, title : "tytuł testowy", addedby : "plajah", status : "enabled" },'.
+					'{ id : 2, title : "tytuł testowy2", addedby : "plajah2", status : "enabled" },'.
+					'{ id : 3, title : "tytuł testowy3", addedby : "plajah3", status : "enabled" },'.
+					'{ id : 4, title : "tytuł testowy4", addedby : "plajah4", status : "enabled" },'.
+					'{ id : 5, title : "tytuł testowy5", addedby : "plajah5", status : "enabled" },'.
+					'{ id : 6, title : "tytuł testowy6", addedby : "plajah6", status : "enabled" }'.
+				']'.
+			'}'.
+		'}';
+		
+		echo $articleList;
+	}
+	
+	public function getcategorylistAction() {
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender();
+		
+		$categoryList = '{'.
+			'result : {'.
+				'items : ['.
+					'{ id : 1, name : "kategoria", addedby : "plajah" },'.
+					'{ id : 2, name : "kategoria2", addedby : "plajah2" },'.
+					'{ id : 3, name : "kategoria3", addedby : "plajah3" },'.
+					'{ id : 4, name : "kategoria4", addedby : "plajah4" },'.
+					'{ id : 5, name : "kategoria5", addedby : "plajah5" },'.
+					'{ id : 6, name : "kategoria6", addedby : "plajah6" }'.
+				']'.
+			'}'.
+		'}';
+		
+		echo $categoryList;
 	}
 }
--- omdform/public/scripts/omd/Omd.Langpl.js
+++ omdform/public/scripts/omd/Omd.Langpl.js
@@ -3,12 +3,86 @@
 	Ext.namespace( 'Omd.Langpl' );
 	
 	var _lang = Omd.Langpl;
+
+	//tytuł zakładki kategorie
+	_lang.TabTitleCategories = 'Kategorie';
 	
 	//tytuł zakładki artykuły
 	_lang.TabTitleArticle = 'Artykuły';
 
-	//tytuł zakładki kategorie
-	_lang.TabTitleCategories = 'Kategorie';
-
+	//tytuł grida artykuły
+	_lang.gridTitleArticle = 'Lista artykułów';
+	
+	//tytuł grida element id
+	_lang.gridTitleItemId = 'Id';
+	
+	//tytuł grida element tytuł
+	_lang.gridTitleItemTitle = 'Tytuł';
+	
+	//tytuł grida element dodany przez
+	_lang.gridTitleItemAddedby = 'Dodany przez';
+	
+	//tytuł grida element status
+	_lang.gridTitleItemStatus = 'Status';
+	
+	//tytuł buttona dodaj kategorie
+	_lang.toolbarTitleAdd = 'Dodaj';
+	
+	//text z tooltipa dla buttona dodaj kategorie
+	_lang.toolbarTextAdd = 'Kliknij ady dodać kategorie';
+	
+	//tytuł buttona zapisz kategorie
+	_lang.toolbarTitleSave = 'Zapisz';
+	
+	//text z tooltipa dla buttona zapisz kategorie
+	_lang.toolbarTextSave = 'Kliknij aby zapisać kategorie';
+	
+	//tytuł buttona edytuj kategorie
+	_lang.toolbarTitleEdit = 'Edytuj';
+	
+	//text z tooltipa dla buttona edytuj kategorie
+	_lang.toolbarTextEdit = 'Kliknij aby edytować kategorie';
+	
+	//tytuł buttona anuluj kategorie
+	_lang.toolbarTitleCancel = 'Anuluj';
+	
+	//text z tooltipa dla buttona anuluj kategorie
+	_lang.toolbarTextCancel = 'Kliknij aby anulować kategorie';
+	
+	//tytuł buttona usuń kategorie
+	_lang.toolbarTitleDelete = 'Usuń';
+	
+	//text z tooltipa dla buttona usuń kategorie
+	_lang.toolbarTextDelete = 'Kliknij aby usunąć kategorie';
+	
+	//tytuł buttona dodaj kategorie
+	_lang.toolbarTitleAddCategory = 'Dodaj';
+	
+	//text z tooltipa dla buttona dodaj kategorie
+	_lang.toolbarTextAddCategory = 'Kliknij ady dodać kategorie';
+	
+	//tytuł buttona zapisz kategorie
+	_lang.toolbarTitleSaveCategory = 'Zapisz';
+	
+	//text z tooltipa dla buttona zapisz kategorie
+	_lang.toolbarTextSaveCategory = 'Kliknij aby zapisać kategorie';
+	
+	//tytuł buttona edytuj kategorie
+	_lang.toolbarTitleEditCategory = 'Edytuj';
+	
+	//text z tooltipa dla buttona edytuj kategorie
+	_lang.toolbarTextEditCategory = 'Kliknij aby edytować kategorie';
+	
+	//tytuł buttona anuluj kategorie
+	_lang.toolbarTitleCancelCategory = 'Anuluj';
+	
+	//text z tooltipa dla buttona anuluj kategorie
+	_lang.toolbarTextCancelCategory = 'Kliknij aby anulować kategorie';
+	
+	//tytuł buttona usuń kategorie
+	_lang.toolbarTitleDeleteCategory = 'Usuń';
+	
+	//text z tooltipa dla buttona usuń kategorie
+	_lang.toolbarTextDeleteCategory = 'Kliknij aby usunąć kategorie';
 
 })();
