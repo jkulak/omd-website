621c1f9d07565e90c0727fcc3a013157fcf9eff6
jakub.kulak <jakub.kulak@eabb647c-98d7-11de-9e98-d1f6c90d23ca>
2010-02-09 20:25:12 +0000

    * drobne poprawki formularza komunikacyjnego

--- omd/application/forms/ContactForm.php
+++ omd/application/forms/ContactForm.php
@@ -12,25 +12,25 @@ class forms_ContactForm extends Zend_Form
 	
 				$this->setMethod('post');
 
-				$institution = new Zend_Form_Element_Text('institution', array('required' => true, 'label' => 'Instytucja *', 'class' => 'text'));
+				$institution = new Zend_Form_Element_Text('institution', array('required' => true, 'label' => 'Instytucja', 'class' => 'text'));
 				$institution->addValidator('NotEmpty', true, array ('messages' => array ('isEmpty' => self::VALIDATION_ERROR_EMPTY ) ) );
 	
-				$name = new Zend_Form_Element_Text('name', array('required' => true, 'label' => 'Imię i nazwisko *', 'class' => 'text'));
+				$name = new Zend_Form_Element_Text('name', array('required' => true, 'label' => 'Imię i nazwisko', 'class' => 'text'));
 				$name->addValidator('NotEmpty', true, array ('messages' => array ('isEmpty' => self::VALIDATION_ERROR_EMPTY ) ) );
 	
-				$position = new Zend_Form_Element_Text('position', array('required' => true, 'label' => 'Stanowisko *', 'class' => 'text'));
+				$position = new Zend_Form_Element_Text('position', array('required' => true, 'label' => 'Stanowisko', 'class' => 'text'));
 				$position->addValidator('NotEmpty', true, array ('messages' => array ('isEmpty' => self::VALIDATION_ERROR_EMPTY ) ) );
 	
-				$email = new Zend_Form_Element_Text('email', array('required' => true, 'label' => 'E-mail *', 'class' => 'text'));
+				$email = new Zend_Form_Element_Text('email', array('required' => true, 'label' => 'E-mail', 'class' => 'text'));
 				$email->addValidator('NotEmpty', true, array ('messages' => array ('isEmpty' => self::VALIDATION_ERROR_EMPTY ) ) );
 				$email->addValidator('EmailAddress', true, array ('messages' => array (
 					'emailAddressInvalid' => self::VALIDATION_ERROR_EMAIL_INVALID,
 					'emailAddressInvalidFormat' => self::VALIDATION_ERROR_EMAIL_INVALID_FORMAT ) ) );
 	
-				$telephone = new Zend_Form_Element_Text('telephone', array('required' => true, 'label' => 'Telefon *', 'class' => 'text'));
+				$telephone = new Zend_Form_Element_Text('telephone', array('required' => true, 'label' => 'Telefon', 'class' => 'text'));
 				$telephone->addValidator('NotEmpty', true, array ('messages' => array ('isEmpty' => self::VALIDATION_ERROR_EMPTY ) ) );
 	
-				$message = new Zend_Form_Element_Textarea('message', array('required' => true, 'label' => 'Wiadomość *', 'class' => 'text', 'rows' => 5, 'cols' => 80));
+				$message = new Zend_Form_Element_Textarea('message', array('required' => true, 'label' => 'Wiadomość', 'class' => 'text', 'rows' => 5, 'cols' => 80));
 				$message->addValidator('NotEmpty', true, array ('messages' => array ('isEmpty' => self::VALIDATION_ERROR_EMPTY ) ) );
 	
 				$copy = new Zend_Form_Element_Checkbox('copy', array('label' => 'Prześlij kopię tej wiadomości na swój e-mail'));
